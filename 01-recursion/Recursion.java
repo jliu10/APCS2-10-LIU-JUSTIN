d7ca694939175abbc21a8b9866c739930983251f
Justin Liu <exovitejl@gmail.com>
2021-02-07 13:44:45 -0500

    set up method heads

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,30 @@
+public class Recursion{
+  /*
+  *@param s any string
+  *@return a string that is the reversed version of s, do NOT use built in methods to do so, use recursion.
+  */
+  public static String reverse(String s){
+    return s;
+  }
+
+  /*
+  *@param length how long the words must be
+  *param word the variable to store the partial solution (should start at "")
+  *@return the number of words that have no adjacent matching letters using the letters a-z.
+  *Repetition allowed except when letters are adjacent.
+  */
+  public static long countNoDoubleLetterWords(int length,String word){
+    //Hint: not a wrapper method, but you must call it starting with "" as your word.
+    return 0;
+  }
+
+  /*
+  *@param n any non-negative value you want to take the sqrt of
+  *@return the approximate sqrt of n within a tolerance of 0.001%
+  */
+  public static double sqrt(double n){
+    //Hint: This is a wrapper method.
+    return 0;
+  }
+  
+}
