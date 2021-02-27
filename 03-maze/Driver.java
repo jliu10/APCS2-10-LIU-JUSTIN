2bf3e313a94c77ab8991f733c9bb23841a07fdc8
Justin Liu <exovitejl@gmail.com>
2021-02-27 17:16:10 -0500

    modified Driver from HW website

---
+++ 03-maze/Driver.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Driver{
+
+    public static void main(String[]args){
+      String filename="data1.dat";
+      if(args.length>0) filename = args[0];
+
+      try{
+        Maze f;
+        f = new Maze(filename);//true animates the maze.
+
+        f.setAnimate(true);//comment this if you are using windows cmd/powershell
+        System.out.println(f.solve()+" steps");
+        System.out.println(f);
+      }catch(FileNotFoundException e){
+        System.out.println("Invalid filename: "+filename);
+      }
+    }
+
+}
