70148c00e35e75fefbd320ccbccd8eca05e38f4b
Justin Liu <exovitejl@gmail.com>
2021-03-02 12:24:06 -0500

    added default maze and square maze; allow method generate to be tested

--- 03-maze/Driver2.java
+++ 03-maze/Driver2.java
@@ -3,12 +3,22 @@ public class Driver2{
   public static void main(String[] args){
     int r=0;
     int c=0;
+    if(args.length==0){
+      r=10;
+      c=20;
+    }
+    if(args.length==1){
+      r=Integer.parseInt(args[0]);
+      c=r;
+    }
     if(args.length==2){
       r=Integer.parseInt(args[0]);
       c=Integer.parseInt(args[1]);
     }
 
     MazeGenerator p=new MazeGenerator(r,c);
+    System.out.println(p);
+    generate(p.getMaze());
     System.out.println(p);
   }
 
