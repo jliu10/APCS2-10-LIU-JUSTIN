f8be3a87ad19e766e81f2e117fab3951d7f0c3a8
Justin Liu <exovitejl@gmail.com>
2021-03-02 11:26:26 -0500

    added methods heads: generate, generateAmazing

---
+++ 03-maze/MazeGenerator.java
@@ -0,0 +1,13 @@
+public class MazeGenerator{
+
+  public static void generate(char[][]maze,int startrow,int startcol){
+
+  }
+
+
+
+  public static void generateAmazing(char[][]maze,row,col,startrow,startcol){
+
+  }
+
+}
