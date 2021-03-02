3ba7a0d7d0570efa5a483655e00454f422a847f4
Justin Liu <exovitejl@gmail.com>
2021-03-02 12:00:04 -0500

    tested constructor MazeGenerator

--- 03-maze/Driver2.java
+++ 03-maze/Driver2.java
@@ -1,7 +1,15 @@
 public class Driver2{
 
   public static void main(String[] args){
-    
+    int r=0;
+    int c=0;
+    if(args.length==2){
+      r=Integer.parseInt(args[0]);
+      c=Integer.parseInt(args[1]);
+    }
+
+    MazeGenerator p=new MazeGenerator(r,c);
+    System.out.println(p);
   }
 
 }
