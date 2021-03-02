bc10c016539e3971f26fca9797997fedb3ca9f9b
Justin Liu <exovitejl@gmail.com>
2021-03-02 11:58:05 -0500

    added and wrote: constructor MazeGenerator, method toString (for testing purposes); method generateAmazing: fixed head

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,12 +1,32 @@
 public class MazeGenerator{
 
+  private char[][] maze;
+
+  public MazeGenerator(int r, int c){ // generates an r x c rectangle of #s
+    maze=new char[r][c];
+    for(int i=0; i<r; i++){
+      for(int j=0; j<c; j++) maze[i][j]='#';
+    }
+  }
+
+  public String toString(){
+    String s="";
+    for(int i=0; i<maze.length; i++){
+      for(int j=0; j<maze[i].length; j++){
+        s+=maze[i][j];
+      }
+      if(i<maze.length-1) s+="\n";
+    }
+    return s;
+  }
+
   public static void generate(char[][]maze,int startrow,int startcol){
 
   }
 
 
 
-  public static void generateAmazing(char[][]maze,row,col,startrow,startcol){
+  public static void generateAmazing(char[][]maze,int row,int col,int startrow,int startcol){
 
   }
 
