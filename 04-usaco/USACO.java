25edec6772b435a7ba78a5e0e82d6616c5676e7b
Justin Liu <exovitejl@gmail.com>
2021-03-05 15:59:57 -0500

    method bronze: converted input into arrays and ints, but came across 'error: unreported exception FileNotFoundException'

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -18,12 +18,28 @@ public class USACO{
     return info;
   }
 
-  public static int bronze(String filename) throws FileNotFoundException{
+  public static int bronze(String filename){
+    ArrayList<ArrayList<String>> info=read(filename);
+    // setting up arrays, var E
+    int[][] pasture=new int[Integer.parseInt(info.get(0).get(0))][Integer.parseInt(info.get(0).get(1))];
+    int E=Integer.parseInt(info.get(0).get(2));
+    int[][] stomps=new int[Integer.parseInt(info.get(0).get(3))][3];
+
+    // filling in arrays
+    for(int i=0; i<pasture.length; i++){
+      for(int j=0; j<pasture[0].length; j++){
+        pasture[i][j]=Integer.parseInt(info.get(1+i).get(j));
+      }
+    }
+    for(int i=0; i<stomps.length; i++){
+      for(int j=0; j<3; j++) stomps[i][j]=Integer.parseInt(info.get(i+1+pasture.length).get(j));
+    }
 
     return 0;
   }
 
   public static long silver(String filename){
+    ArrayList<ArrayList<String>> info=read(filename);
     return 0;
   }
 
