ad6bf2aea3fb59f95a4cb7c548ad1c84c6863da4
Justin Liu <exovitejl@gmail.com>
2021-03-08 03:55:09 -0500

    method main: added ability to test parameter inputs

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -2,14 +2,30 @@ import java.io.*;
 public class Driver{
 
   public static void main(String[] args){
-    String filename="lake1.input";
+    String lake1="lake1.input";
+    String cowPath1="cowPath1.input";
+
+    String input="";
+    if(args.length==2){
+      input=args[1];
+    }
 
     try{
-      System.out.println(USACO.read(filename).toString());
-      System.out.println(USACO.bronze(filename));
-
+      System.out.println(USACO.read(lake1).toString());
+      System.out.println(lake1+": "+USACO.bronze(lake1));
+      System.out.println(USACO.read(cowPath1).toString());
+      System.out.println(cowPath1+": "+USACO.silver(cowPath1));
+      System.out.println();
+      if(args.length==2){
+        if(args[0].equals("bronze")){
+          System.out.println("bronze "+input+": "+USACO.bronze(input));
+        }
+        else if(args[0].equals("silver")){
+          System.out.println("silver "+input+": "+USACO.silver(input));
+        }
+      }
     }catch(FileNotFoundException e){
-      System.out.println("Invalid filename: "+filename);
+      System.out.println("Invalid filename");
     }
   }
 
