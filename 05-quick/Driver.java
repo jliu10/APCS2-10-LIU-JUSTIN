c27956a0f6eb90ddaa505e9bb6e9fc7606886a61
Justin Liu <exovitejl@gmail.com>
2021-03-10 23:36:50 -0500

    tested partition for all indices of array given on HW website

--- 05-quick/Driver.java
+++ 05-quick/Driver.java
@@ -1,9 +1,21 @@
+import java.util.*;
 public class Driver{
 
   public static void main(String[] args){
     int[] a1=new int[]{999,999,999,4,3,2,1,0,999,999,999};
 
-
+    System.out.println(Preliminary.toString(a1));
+    System.out.println("Index: "+Preliminary.partition(a1,7,10));
+    System.out.println(Preliminary.toString(a1));
+    System.out.println();
+    /*
+    Preliminary.swap(a1,2,5);
+    System.out.println(Preliminary.toString(a1));
+    Preliminary.swap(a1,2,5);
+    System.out.println(Preliminary.toString(a1));
+    Preliminary.swap(a1,0,0);
+    System.out.println(Preliminary.toString(a1));
+    */
   }
 
 }
