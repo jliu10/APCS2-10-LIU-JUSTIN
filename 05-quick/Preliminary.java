5dbe693ef87ce5f855db47591d5af592d3aa8515
Justin Liu <exovitejl@gmail.com>
2021-03-10 19:42:12 -0500

    added and wrote methods swap, toString

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,5 +1,19 @@
 public class Preliminary{
 
+  public static void swap(int[] data, int i, int j){
+    int t=data[j];
+    data[j]=data[i];
+    data[i]=t;
+  }
+
+  public static String toString(int[] data){
+    String s="{";
+    for(int i=0; i<data.length-1; i++) s+=data[i]+", ";
+    if(data.length>0) s+=data[data.length-1];
+    s+="}";
+    return s;
+  }
+
   /*Modify the array such that:
   *1. A random index from start to end inclusive is chosen, the corresponding
   *   element is designated the pivot element.
@@ -10,9 +24,10 @@ public class Preliminary{
   *4. Only the indices from start to end inclusive are considered in range
   *@return the index of the final position of the pivot element.
   */
-  public static int partition ( int [] data, int start, int end){
+  public static int partition(int[] data, int start, int end){
     int r=(int)(Math.random()*(end-start+1))+start;
-    return r;
+    int p=data[r];
+    return p;
   }
 
 }
