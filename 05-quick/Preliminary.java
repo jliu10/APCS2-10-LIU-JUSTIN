ec27211637667f3ea783a1d9f4e430f8c1f7cce7
Justin Liu <exovitejl@gmail.com>
2021-03-10 19:19:36 -0500

    method partition: wrote random index generator

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,7 +1,18 @@
 public class Preliminary{
 
+  /*Modify the array such that:
+  *1. A random index from start to end inclusive is chosen, the corresponding
+  *   element is designated the pivot element.
+  *2. all elements in range that are smaller than the pivot element are placed
+      before the pivot element.
+  *3. all elements in range that are larger than the pivot element are placed
+      after the pivot element.
+  *4. Only the indices from start to end inclusive are considered in range
+  *@return the index of the final position of the pivot element.
+  */
   public static int partition ( int [] data, int start, int end){
-    return 0;
+    int r=(int)(Math.random()*(end-start+1))+start;
+    return r;
   }
 
 }
