ed2709a67ff0fd27b1d22ed796f7c2ec2e327fa8
Justin Liu <exovitejl@gmail.com>
2021-03-13 18:15:00 -0500

    made changes to reflect removal of median system

--- 05-quick/Driver.java
+++ 05-quick/Driver.java
@@ -1,11 +1,6 @@
 import java.util.*;
 public class Driver{
 
-  public static int testMedian(int[] data){
-    System.out.println("Median of "+Preliminary.toString(data)+": ");
-    return Preliminary.median(data[0],data[1],data[2]);
-  }
-
   public static void main(String[] args){
     int[] a1=new int[]{999,999,999,4,3,2,1,0,999,999,999};
     int[] a2=new int[99];
@@ -19,21 +14,8 @@ public class Driver{
     System.out.println(Preliminary.toString(a2));
     System.out.println();
 
-    int[] m1=new int[]{1,2,3};
-    System.out.println(testMedian(m1));
-    m1=new int[]{3,2,1};
-    System.out.println(testMedian(m1));
-    m1=new int[]{2,1,3};
-    System.out.println(testMedian(m1));
-    m1=new int[]{2,2,2};
-    System.out.println(testMedian(m1));
-    m1=new int[]{3,2,3};
-    System.out.println(testMedian(m1));
-    m1=new int[]{2,3,2};
-    System.out.println(testMedian(m1));
-
     System.out.println(Preliminary.toString(a1));
-    System.out.println("Index: "+Preliminary.partition(a1,7,10));
+    System.out.println("Index: "+Preliminary.partition(a1,0,0));
     System.out.println(Preliminary.toString(a1));
     System.out.println();
   }
