ce9f1a65e11e63b35760a1624bdbe59bba8fc436
Justin Liu <exovitejl@gmail.com>
2021-03-14 15:47:24 -0400

    further tested method quickselect. noticed that when finding nth smallest element of arr, runs fast for when n is near either end, but much slower for when n is closer to middle

--- 05-quick/Driver2.java
+++ 05-quick/Driver2.java
@@ -1,23 +1,37 @@
+import java.util.*;
 public class Driver2{
 
   public static void testSelect(int[] a, int k, int ex){
-    System.out.println("quickselect("+Quick.toString(a)+", "+k+") should return: "+ex);
-    System.out.println("\treturns: "+Quick.quickselect(a,k));
-    System.out.println("\tarray after: "+Quick.toString(a));
+  //  System.out.println("quickselect("+Quick.toString(a)+", "+k+") should return: "+ex);
+    System.out.println("  returns: "+Quick.quickselect(a,k));
+  //  System.out.println("  array after: "+Quick.toString(a));
     System.out.println();
+  }
+
+  public static void randomize(int[] a, int lo, int hi){
+    for(int i=0; i<a.length; i++){
+      a[i]=(int)(Math.random()*(hi-lo+1))+lo;
+    }
   }
 
   public static void main(String[] args){
     int[] arr  = new int[]{2, 10, 15, 23, 0, 5};
+    int size=1000;
+    if(args.length==1){
+      size=Integer.parseInt(args[0]);
+    }
     //If sorted: {0,  2,  5, 10, 15, 23}
     //since we are modifying the array you would not call this repeatedly.
     //This is to show what the returned value would be in all cases.
-
+    /*
     System.out.println(Quick.toString(arr));
     System.out.println("Index: "+Quick.partition(arr,0,5));
     System.out.println(Quick.toString(arr));
     System.out.println();
+    */
 
+    /*
+    System.out.println("\n-----QUICKSELECT: TESTING GIVEN EXAMPLES-----");
     testSelect(arr, 0, 0);
     arr  = new int[]{2, 10, 15, 23, 0,  5};
     testSelect(arr, 1, 2);
@@ -29,7 +43,22 @@ public class Driver2{
     testSelect(arr, 4, 15);
     arr  = new int[]{2, 10, 15, 23, 0,  5};
     testSelect(arr, 5, 23);
-
+    */
+    System.out.println("\n-----QUICKSELECT: TESTING RANDOM EXAMPLES-----");
+    int[] arr1=new int[size];
+    randomize(arr1,0,100);
+    int[] arr2=new int[arr1.length];
+    for(int i=0; i<arr1.length; i++) arr2[i]= arr1[i];
+    int n=(int)(Math.random()*100);
+    // System.out.println("n: "+n);
+    long beg = System.currentTimeMillis();
+    Arrays.sort(arr2);
+    long end = System.currentTimeMillis();
+    System.out.println("Arrays.sort time: "+(end-beg));
+    beg = System.currentTimeMillis();
+    testSelect(arr1,10,-1);
+    end = System.currentTimeMillis();
+    System.out.println("quickselect time: "+(end-beg));
   }
 
 }
