e1bc02d7bb6bc5b2c190435e9e131f1731d20c06
Justin Liu <exovitejl@gmail.com>
2021-03-17 14:04:44 -0400

    added method heads

---
+++ 06-merge/Merge.java
@@ -0,0 +1,15 @@
+public class Merge{
+
+  public static void merge(int[] data){
+    
+  }
+
+  public static void mergesort(int[] data){
+
+  }
+
+  public static void mergesort(int[] data, int[] temp, int lo, int hi){
+
+  }
+
+}
