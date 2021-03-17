27b6461107aef89593863730e566f1d9f789abc4
Justin Liu <exovitejl@gmail.com>
2021-03-17 14:20:14 -0400

    added and wrote method toString

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,5 +1,13 @@
 public class Merge{
 
+  public static String toString(int[] data){
+    String s="{";
+    for(int i=0; i<data.length-1; i++) s+=data[i]+", ";
+    if(data.length>0) s+=data[data.length-1];
+    s+="}";
+    return s;
+  }
+
   public static int[] merge(int[] data, int lo1, int hi1, int lo2, int hi2){
 
   }
