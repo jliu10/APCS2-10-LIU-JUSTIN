a06e6d995ea9cd7c7ee8ea3ab80d7b30f151343a
Justin Liu <exovitejl@gmail.com>
2021-03-18 10:59:41 -0400

    tested method merge

--- 06-merge/Driver.java
+++ 06-merge/Driver.java
@@ -6,10 +6,15 @@ public class Driver{
 
   public static void main(String[] args){
     int[] arr=new int[]{38,27,43,3,9,82,10};
+    int[] res=new int[7];
     System.out.println(toString(arr));
-    // System.out.println(toString(Merge.merge(arr,0,0, 0,0)));
-    // System.out.println(toString(Merge.merge(arr,0,0, 1,1)));
-    System.out.println(toString(Merge.merge(arr,1,2, 3,4)));
+    // Merge.merge(arr,res, 0,0, 0,0);
+    // Merge.merge(arr,res, 0,0, 1,1);
+    // Merge.merge(arr,res, 0,1, 2,3);
+    // Merge.merge(arr,res, 1,2, 3,4);
+    Merge.merge(arr,res, 0,0, 1,2);
+
+    System.out.println(toString(res));
   }
 
 }
