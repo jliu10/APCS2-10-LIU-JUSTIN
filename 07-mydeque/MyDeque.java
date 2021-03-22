54f3576daa2884a0ba65ffa685d08fc9750880e3
Justin Liu <exovitejl@gmail.com>
2021-03-22 09:15:39 -0400

    fixed method toString

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -36,19 +36,23 @@ public class MyDeque<E> {
     }
 
     public String stringify(){
-      String s="{";
-      for(int i=0; i<data.length-1; i++) s+=data[i]+", ";
-      if(data.length>0) s+=data[data.length-1];
-      s+="}";
+      String s = "{";
+      for(int i = 0; i < data.length - 1; i++) s += data[i] + ", ";
+      if(data.length > 0) s += data[data.length - 1];
+      s += "}";
+      System.out.println("start: " + start);
+      System.out.println("end: " + end);
+      System.out.println("getPrev: " + getPrev(9));
+      System.out.println("getNext: " + getNext(98));
       return s;
     }
 
     public String toString() {
       String s = "{";
       if(end != -1) {
-        for(int i = start - 1; i != getPrev(end); i++) {
-           // if(i>=data.length) i=0;
-           // s+=data[i]+", ";
+        for(int i = start - 1; i != getPrev(end); i = getNext(i)) {
+           // System.out.println("i: " + i);
+           // System.out.println("getNext: " + getNext(i));
            s += data[getNext(i)] + ", ";
         }
         s += data[end];
