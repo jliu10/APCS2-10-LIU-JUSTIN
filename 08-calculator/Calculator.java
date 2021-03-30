190ab529af26a7822c5ac6d00c01b754448ec0d3
Justin Liu <exovitejl@gmail.com>
2021-03-30 14:48:04 -0400

    set up class Calculator

---
+++ 08-calculator/Calculator.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+
+public class Calculator {
+  /*Evaluate a postfix expression stored in s.
+  *Assume valid postfix notation, of ints doubles and operators separated by spaces.
+  *Valid operators are + - / * and % (remainder not modulo)
+  *All results are doubles even if the operands are both int.
+  *@throws IllegalArgumentException when there are too many or too few operands.
+  *        Use the string parameter of your exception to indicate what happened.
+  */
+  public static double eval(String s) {
+    return -1;
+  }
+}
