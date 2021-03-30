ff8f30cbe23bbbb02a0aae4faeedb719cfad04ee
Justin Liu <exovitejl@gmail.com>
2021-03-30 16:15:37 -0400

    method eval: converted String input into tokens

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,6 +1,7 @@
 // import java.util.*;
 
 public class Calculator {
+
   /*Evaluate a postfix expression stored in s.
   *Assume valid postfix notation, of ints doubles and operators separated by spaces.
   *Valid operators are + - / * and % (remainder not modulo)
@@ -9,6 +10,18 @@ public class Calculator {
   *        Use the string parameter of your exception to indicate what happened.
   */
   public static double eval(String s) {
+    if(s.length() == 0) throw new IllegalArgumentException("input is empty");
+    String[] tokens = s.split(" ");
+    // System.out.println(tokens[0]+" "+tokens[1]);
+    String ops = "+-/*%";
+    int operators = 0;
+    for(String t : tokens) if(ops.contains(t)) operators++;
+    int operands = tokens.length - operators;
+    System.out.println("operators: "+operators);
+    System.out.println("operands: "+operands);
+
+    if(false) throw new IllegalArgumentException("too many operands");
+    if(false) throw new IllegalArgumentException("too few operands");
     return -1;
   }
 }
