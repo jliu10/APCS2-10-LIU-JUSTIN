c142250fb5b72ddd7a267dc997fb9def4a61db2e
Justin Liu <exovitejl@gmail.com>
2021-04-15 12:24:14 -0400

    created class Frontier with constructor and methods

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Frontier {
+   private Queue<int[]> frontier;
+
+   public Frontier() {
+     frontier = new ArrayDeque<int[]>();
+   }
+
+   public int size() {
+
+   }
+
+   public void add(int[]location) {
+
+   }
+
+   public int[] remove() {
+
+   }
+}
