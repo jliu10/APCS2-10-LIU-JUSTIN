fd0f5540cfff8690fef316aba0d1f7d848ad1eb4
Justin Liu <exovitejl@gmail.com>
2021-04-23 17:44:17 -0400

    added and wrote method toString

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,5 +1,14 @@
 public class MyHeap {
 
+  public static String toString(int[] data, int size) {
+    String s = "{";
+    for(int i = 0; i < size - 1; i++) {
+      s += data[i] + ", ";
+    }
+    if(size > 0) s += data[size - 1];
+    return s + "}";
+  }
+
  /*Swap the element at the provided index downward into the correct position.
    This will swap with the larger of the child nodes provided thatchild is larger.
    This stops when a leaf is reached, or neither child is larger.
@@ -9,14 +18,14 @@ public class MyHeap {
   *@precondition index is between 0 and size-1 inclusive
   *@precondition size is between 0 and data.length inclusive.
   */
- private static void pushDown(int[]data, int size, int index) {
+ private static void pushDown(int[] data, int size, int index) {
 
  }
 
  /*Reorder the provided array to be a valid heap.
   *@param data is the array to be modified
   */
- private static void buildHeap(int[]data) {
+ private static void buildHeap(int[] data) {
 
  }
 
@@ -24,15 +33,15 @@ public class MyHeap {
   *Then push the new root down, but not past the index that it swapped with.
   *@precondition: size is between 0 and data.length inclusive.
   */
- private static void remove(int[]data,int size) {
+ private static void remove(int[] data, int size) {
 
  }
 
  /*Sort the provided array
   *@param data is the array to be sorted
   */
- public static void heapsort(int[]data) {
-   
+ public static void heapsort(int[] data) {
+
  }
 
 }
