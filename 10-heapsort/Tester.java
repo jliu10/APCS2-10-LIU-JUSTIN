2fbdc30a0b96e0fa6a74673b4ff6bafa0f1a0f34
Justin Liu <exovitejl@gmail.com>
2021-04-23 18:53:56 -0400

    added and wrote method testPushDown

---
+++ 10-heapsort/Tester.java
@@ -0,0 +1,24 @@
+public class Tester {
+
+  public static void testPushDown(int[] data, int size, int index) {
+    System.out.println("TESTING pushdown:");
+    System.out.println("\tdata: ");
+    if(size <= 20) { // don't wanna print too large of an array
+      System.out.print(MyHeap.toString(data, size));
+    }
+    else System.out.print("over size 20");
+    System.out.println("\tsize: " + size);
+    System.out.println("\tindex: " + index);
+    MyHeap.pushDown(data, size, index);
+    if(size <= 20) { // don't wanna print too large of an array
+      System.out.print(MyHeap.toString(data, size));
+    }
+    else System.out.print("over size 20");
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
