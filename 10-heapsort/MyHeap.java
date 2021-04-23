ff43283599fd5ad1a6d9002974a443d83dadd319
Justin Liu <exovitejl@gmail.com>
2021-04-23 16:49:33 -0400

    created class MyHeap; copied method heads and comments

---
+++ 10-heapsort/MyHeap.java
@@ -0,0 +1,38 @@
+public class MyHeap {
+
+ /*Swap the element at the provided index downward into the correct position.
+   This will swap with the larger of the child nodes provided thatchild is larger.
+   This stops when a leaf is reached, or neither child is larger.
+   *@param size the number of heap elements in the data array.
+           This is needed to allow a partially full array to be provided.
+  *@precondition the children of data[index] are valid heaps.
+  *@precondition index is between 0 and size-1 inclusive
+  *@precondition size is between 0 and data.length inclusive.
+  */
+ private static void pushDown(int[]data, int size, int index) {
+
+ }
+
+ /*Reorder the provided array to be a valid heap.
+  *@param data is the array to be modified
+  */
+ private static void buildHeap(int[]data) {
+
+ }
+
+ /*Swap the root node with the element at the provided index.
+  *Then push the new root down, but not past the index that it swapped with.
+  *@precondition: size is between 0 and data.length inclusive.
+  */
+ private static void remove(int[]data,int size) {
+
+ }
+
+ /*Sort the provided array
+  *@param data is the array to be sorted
+  */
+ public static void heapsort(int[]data) {
+   
+ }
+
+}
